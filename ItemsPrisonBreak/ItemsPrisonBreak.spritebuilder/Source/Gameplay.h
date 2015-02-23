0e3997233feb665a4dd5d31662c5b811486cddbc
Brandon2010 <wxbrandon2010@gmail.com>
2015-02-22 23:55:32 -0500

    Finish throwing function

--- ItemsPrisonBreak/ItemsPrisonBreak.spritebuilder/Source/Gameplay.h
+++ ItemsPrisonBreak/ItemsPrisonBreak.spritebuilder/Source/Gameplay.h
@@ -1,13 +1,13 @@
 //
-//  Stone.m
+//  Gameplay.h
 //  ItemsPrisonBreak
 //
 //  Created by Brandon on 2/22/15.
 //  Copyright (c) 2015 Apportable. All rights reserved.
 //
 
-#import "Stone.h"
+#import "CCNode.h"
 
-@implementation Stone
+@interface Gameplay : CCNode
 
 @end
---
+++ ItemsPrisonBreak/ItemsPrisonBreak.spritebuilder/Source/Gameplay.m
@@ -0,0 +1,38 @@
+//
+//  Gameplay.m
+//  ItemsPrisonBreak
+//
+//  Created by Brandon on 2/22/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+@implementation Gameplay {
+    CCPhysicsNode *_physicsNode;
+    CCNode *_escaperHand;
+}
+
+// is called when CCB file has completed loading
+- (void)didLoadFromCCB {
+    self.userInteractionEnabled = TRUE;
+}
+
+// called on every touch in this scene
+- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+    [self launchStone];
+}
+
+- (void)launchStone {
+    CCNode* stone = [CCBReader load:@"Stone"];
+    stone.position = ccpAdd(_escaperHand.position, ccp(16, 15));
+    stone.scale = 0.5;
+    
+    [_physicsNode addChild:stone];
+    
+    CGPoint launchDirection = ccp(1, 0);
+    CGPoint force = ccpMult(launchDirection, 8000);
+    [stone.physicsBody applyForce:force];
+}
+
+@end
