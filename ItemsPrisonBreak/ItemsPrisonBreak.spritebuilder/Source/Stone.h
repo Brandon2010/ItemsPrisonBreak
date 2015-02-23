c0fc46fcfb904731c772d4607183dd8cbda243f0
Brandon2010 <wxbrandon2010@gmail.com>
2015-02-22 23:30:07 -0500

    Create Stone

---
+++ ItemsPrisonBreak/ItemsPrisonBreak.spritebuilder/Source/Stone.h
@@ -0,0 +1,13 @@
+//
+//  Stone.h
+//  ItemsPrisonBreak
+//
+//  Created by Brandon on 2/22/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Stone : CCSprite
+
+@end
---
+++ ItemsPrisonBreak/ItemsPrisonBreak.spritebuilder/Source/Stone.m
@@ -0,0 +1,22 @@
+//
+//  Stone.m
+//  ItemsPrisonBreak
+//
+//  Created by Brandon on 2/22/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Stone.h"
+
+@implementation Stone
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Stone created");
+    }
+    return self;
+}
+
+@end
