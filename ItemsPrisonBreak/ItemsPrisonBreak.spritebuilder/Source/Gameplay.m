18d613536f8a9aac9bb0d6de129efc46079f9a09
Brandon2010 <wxbrandon2010@gmail.com>
2015-02-24 17:00:28 -0500

    Finish sprint and throw but still has some problems when throwing stones

--- ItemsPrisonBreak/ItemsPrisonBreak.spritebuilder/Source/Gameplay.m
+++ ItemsPrisonBreak/ItemsPrisonBreak.spritebuilder/Source/Gameplay.m
@@ -13,7 +13,12 @@
     CCPhysicsNode *_physicsNode;
     CCNode *_escaperHand;
     CCNode *_levelNode;
+    CCNode *_contentNode;
     CCNode *_pullbackNode;
+    CCNode *_mouseJointNode;
+    CCPhysicsJoint *_mouseJoint;
+    CCNode *_currentStone;
+    CCPhysicsJoint *_stoneHandJoint;
 }
 
 // is called when CCB file has completed loading
@@ -28,7 +33,49 @@
 
 // called on every touch in this scene
 - (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
-    [self launchStone];
+    CGPoint touchLocation = [touch locationInNode:_contentNode];
+    
+    // start hand dragging when a touch inside of the hand occurs
+    if (CGRectContainsPoint([_escaperHand  boundingBox], touchLocation))
+    {
+        // move the mouseJointNode to the touch position
+        _mouseJointNode.position = touchLocation;
+        
+        // setup a spring joint between the mouseJointNode and the escaperHand
+        _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_escaperHand.physicsBody anchorA:ccp(0, 0) anchorB:ccp(9.8, 9.3) restLength:0.f stiffness:3000.f damping:60.f];
+        
+        // create a stone from the ccb-file
+        _currentStone = [CCBReader load:@"Stone"];
+        // initially position it on the scoop. 34,138 is the position in the node space of the _catapultArm
+        CGPoint stonePosition = [_escaperHand convertToWorldSpace:ccp(20.5, 35.5)];
+        // transform the world position to the node space to which the penguin will be added (_physicsNode)
+        _currentStone.position = [_physicsNode convertToNodeSpace:stonePosition];
+        _currentStone.scale = 0.5;
+        // add it to the physics world
+        [_physicsNode addChild:_currentStone];
+        // we don't want the penguin to rotate in the scoop
+        _currentStone.physicsBody.allowsRotation = FALSE;
+        
+        // create a joint to keep the penguin fixed to the scoop until the catapult is released
+        _stoneHandJoint = [CCPhysicsJoint connectedPivotJointWithBodyA:_currentStone.physicsBody bodyB:_escaperHand.physicsBody anchorA:_currentStone.anchorPointInPoints];
+    }
+}
+
+- (void)touchMoved:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+{
+    // whenever touches move, update the position of the mouseJointNode to the touch position
+    CGPoint touchLocation = [touch locationInNode:_contentNode];
+    _mouseJointNode.position = touchLocation;
+}
+
+- (void)touchEnded:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+{
+    [self releaseHand];
+}
+
+- (void)touchCancelled:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+{
+    [self releaseHand];
 }
 
 - (void)launchStone {
@@ -46,8 +93,38 @@
 - (BOOL)ccPhysicsCollisionBegin:(CCPhysicsCollisionPair *)pair stone:(CCNode *)stone redswitch :(CCNode *)redswitch {
     [redswitch removeFromParent];
     [stone removeFromParent];
-    CCLOG(@"collision");
     return YES;
+}
+
+- (BOOL)ccPhysicsCollisionBegin:(CCPhysicsCollisionPair *)pair stone:(CCNode *)stone yellowswitch :(CCNode *)yellowswitch {
+    [yellowswitch removeFromParent];
+    [stone removeFromParent];
+    return YES;
+}
+
+
+- (void)releaseHand {
+    if (_mouseJoint != nil)
+    {
+        // releases the joint and lets the catapult snap back
+        [_mouseJoint invalidate];
+        _mouseJoint = nil;
+        
+        // releases the joint and lets the penguin fly
+        [_stoneHandJoint invalidate];
+        _stoneHandJoint = nil;
+        
+        // after snapping rotation is fine
+        _currentStone.physicsBody.allowsRotation = TRUE;
+        
+        // follow the flying penguin
+        //CCActionFollow *follow = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
+        //[_contentNode runAction:follow];
+        // follow the flying penguin
+//        _followPenguin = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
+//        [_contentNode runAction:_followPenguin];
+//        _currentStone.launched = TRUE;
+    }
 }
 
 @end
